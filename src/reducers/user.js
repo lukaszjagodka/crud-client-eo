4194f0ca72249b9968de8762a7e5393fc19ae143
encorey <lukasz.jagodka@protonmail.com>
2020-02-25 19:56:37 +0100

    Added reducer state

---
+++ src/reducers/user.js
@@ -0,0 +1,43 @@
+// import { ADD_USER_FROM_LOGIN} from '../actions/types'
+
+const initialState = {
+    email: '',
+    password: '',
+    token: '',
+    isAuthenticated: false
+}
+
+export const userReducer = (state = initialState, action) => {
+    switch (action.type){
+        // case ADD_USER_FROM_LOGIN:
+        //     return {
+        //         ...state,
+        //         userFromLogin: {
+        //             email: action.value.email,
+        //             password: action.value.password
+        //         },
+        //     }
+        case 'ADD_EMAIL_FROM_LOGIN':
+            // console.log(action.value)
+            return {
+                ...state,
+                email: action.value
+            }
+        case 'ADD_PASSWORD_FROM_LOGIN':
+            // console.log(action.value)
+            return {
+                ...state,
+                password: action.value
+            }
+        // case 'ADD_TOKEN_TO_STATE':
+        //     console.log(action.value, 'token')
+        //     return {
+        //         ...state,
+        //         token: action.value
+        //     }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
