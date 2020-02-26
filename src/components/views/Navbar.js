880b4e5ad3a3dea71d93a3da2a94c54ab959a5fd
encorey <lukasz.jagodka@protonmail.com>
2020-02-26 15:32:46 +0100

    Modified Navbar component

--- src/components/views/Navbar.js
+++ src/components/views/Navbar.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 const Navbar = () => {
+    const token = localStorage.getItem('jwt');
     return(
+        token ? 
         <nav>
             <div className="nav-wrapper" style={{padding: '20px'}}>
                 <ul id="nav-mobile">
-                    {/* <li><Link to="/">Home</Link></li>
-                    <li><Link to="/testpage"> Test </Link></li> */}
-                    {/* <Link to="/account">Profile</Link> */}
-                    {/* <li><Link to="/logout">Logout </Link></li>
-                    <li><Link to="/login">Login</Link></li> */}
                     <li><a href="/">Home</a></li>
                     <li><a href="/testpage">Test</a></li>
                     <li><a href="/logout">Logout</a></li>
-                    <li><a href="/login">Login</a></li>
                 </ul>
             </div>
+        </nav> :
+        <nav>
+        <div className="nav-wrapper" style={{padding: '20px'}}>
+            
+            <ul id="nav-mobile">
+            <h1>LogIn</h1>
+            </ul>
+        </div>
         </nav>
     )
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+    
+}
+
+export default connect(mapStateToProps, null)(Navbar)
